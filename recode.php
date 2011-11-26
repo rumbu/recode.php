f9145d17f151bd19e9f1f1e19406814f0b19ad9f
Roman Lisin <ruplexer@gmail.com>
2011-11-26 15:49:39 +0700

    Main file added

---
+++ recode.php
@@ -0,0 +1,42 @@
+<?php
+/**
+  * @usage
+  * php recode.php -p www
+  */
+
+// ищем файлы
+function browseCall($dir, $regex, $cb) {
+ foreach(glob(chop($dir,'/')."/*") as $file) {
+   if (is_dir($file)) browseCall($file, $regex, $cb);
+   if (is_file($file) && preg_match($regex, basename($file)))
+     call_user_func_array($cb, array($file));
+ }
+}
+// заменяем кодировку
+function myRecode($file) {
+ $s = file_get_contents($file);
+ $e = detectEncoding($s);
+ echo "[".$e."] $file";
+ if ('windows-1251'==$e) {
+   $r = iconv('windows-1251','utf-8',$s);
+   file_put_contents($file, $r);
+   echo " [written ".strlen($r)." bytes]\n";
+ }
+ else echo "\n";
+}
+// определялка кодировки
+function detectEncoding($string) {
+ static $list = array('utf-8', 'windows-1251');
+ foreach ($list as $item) {
+   $sample = @iconv($item, $item, $string);
+   if (md5($sample) == md5($string))
+     return $item;
+ }
+ return null;
+}
+
+if (preg_match("~-p ([^ ]+)~", implode(' ',$argv), $matches)) {
+ $path = $matches[1];
+ browseCall($path, "~\.php$|\.tpl$|\.conf$|\.inc$|\.js$~", "myRecode");
+}
+?>
